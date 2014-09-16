f85140e1fd6a41caf2da27fa1a500b91ac66f52b
takahi-i <takahiko03@gmail.com>
2014-09-16 13:37:46 +0900

    Initial commit

---
+++ src/main/java/cc/redpen/validator/sentence/HankakuKanaValidator.java
@@ -0,0 +1,13 @@
+package cc.redpen.validator.sentence;
+
+import cc.redpen.validator.Validator;
+import cc.redpen.ValidationError;
+import cc.redpen.model.Sentence;
+
+import java.util.List;;
+
+public class HankakuKanaValidator extends Validator<Sentence> {
+    public List<ValidationError> validate(Sentence sentence) {
+        return null;
+    }
+}
